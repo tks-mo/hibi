fe3940f212d80687e924f47fb0aa53a817dca437
___mo <0.km.mo.tk@gmail.com>
2020-11-10 17:23:25 +0000

    作業NO.35

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -34,7 +34,7 @@
                     
                     <input type="hidden" name="selectedDate" value="{{ $selectedDate }}">
                     
-                    @if($diary['image_path'] != null)
+                    @if($diary != null)
                         <div class="form-group row">
                             <div class="col-md-8 mx-auto">
                                 <img src="{{ $diary->image_path }}" class="img-fluid">
